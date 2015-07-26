9719dbc24ac1237c22145d980e873aa9c28ce99b
Bryan Mahoney <BryanMahoney94@yahoo.com>
2015-07-26 18:58:11 -0500

    Reformatted test.py to display things better.

--- Python/B_FEM.py
+++ Python/B_FEM.py
@@ -4,29 +4,50 @@ B_FEM.py is the primary module for my Finite Element project.
 """
 import numpy as np
 import scipy as sp
-from math import *
-	
+import math
+from sys import exit
 	
 class Element(object):
 	"""
 	A small 1-D piece of a structural element which has 6 DOF within
 	the plane.
 	"""
-	def __init__(self,start,stop):
-		self.length = sqrt((stop[1] - start[1])**2 + (stop[0] - start[0])**2)	# Element length
+	# bend_stiff is E*I, the bending stiffness
+	
+	def __init__(self,start,stop,connectivity,bend_stiff):
+		self.length = math.sqrt((stop[1] - start[1])**2 + (stop[0] - start[0])**2)	# Element length
+		self.connectivity = connectivity
+		self.bend_stiff = bend_stiff
+		# (u1,v1,theta1,u2,v2,theta2)
+		self.disp = (0.0,0.0,0.0,0.0,0.0,0.0)
+	def __str__(self):
+		return """
+length = %.4f
+connectivity = %s
+bending stiffness = %.4f
+displacements = %s
+""" % (self.length, str(self.connectivity),self.bend_stiff,str(self.disp))
+	
 
+
+	
 class Beam(object):
 	"""
-	A generic, 1-D structural element which can support both axial
-	and shear loads.
+	A 1-D structural element which can support
+	shear loads.
 	"""
 	def __init__(self,num_elements,coordinates):
+		print "number of elements: %s" % num_elements
+		elements = []
 		for i in range(0,num_elements):
 			print "Element %d says hello!" % i
+			
 		
 class Bar(Beam):
 	"""
 	A 1-D structural element which can only support axial loads.
 	"""
-	pass
+	def __init__(self):
+		print "This class is not yet implemented!"
+		exit()
 
--- Python/test.py
+++ Python/test.py
@@ -6,15 +6,25 @@ import numpy as np
 from B_FEM import *
 
 fid = open('input.txt','r')
-print fid
+print "Printing the file identifier"
+print "-" * 50
+print fid 
+
+print "\n"
+
+print "Printing file contents"
+print "-" * 50
 text = fid.read()
 print text
 
+
 lines = text.splitlines()
 
-test_element = Element((1.0,0.0),(2.0,0.0))
-print "Element length: %.4f" % test_element.length
+test_element = Element((1.0,0.0),(2.0,0.0),(1,2),5*10**6)
 
+print "\nPrinting a test element's attributes"
+print "-" * 50
+print test_element
 test_beam = Beam(2,((0,0),(1,0)))
 
 
